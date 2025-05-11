a63ce85784fedaa406279974b749128f47a2bf40
abujaher5 <abujaher5120@gmail.com>
2025-05-11 08:54:16 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="text-center flex justify-center items-center gap-2">
+        <h1 className="text-cyan-300 text-7xl font-bold  text-center mt-10 ">
+          Vite + React
+        </h1>
+        <h1 className="text-9xl">hi</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
